13df28e0c71e5b8e575802b52bacb073de39a345
Raymond <163283133+Raymzzz808@users.noreply.github.com>
2025-06-25 14:03:19 -1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/hawaii-photo/', // 👈 this is the magic line!
+  base: '/', // 👈 this is the magic line!
   plugins: [react()],
 });
