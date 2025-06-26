3042360a2a5db485e52053e038ad09bf8f61ee42
Raymond <163283133+Raymzzz808@users.noreply.github.com>
2025-06-25 14:53:24 -1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Use root path for custom domain
+  base: './',  // Use root path for custom domain
   plugins: [react()],
   build: {
     rollupOptions: {
